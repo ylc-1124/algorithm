7b1e22f0704cd4dcdd68f47231474b367e30c8b5
ylc-1124 <853703920@qq.com>
2021-08-18 15:12:56 +0800

    路径分裂和路径减半

--- 02-并查集/src/com/ylc/Main.java
+++ 02-并查集/src/com/ylc/Main.java
@@ -12,6 +12,9 @@ public class Main {
         testTime(new UnionFind_QU_S(COUNT));
         testTime(new UnionFind_QU_R(COUNT));
         testTime(new UnionFind_QU_R_PC(COUNT));
+        testTime(new UnionFind_QU_R_PS(COUNT));
+        testTime(new UnionFind_QU_R_PH(COUNT));
+
     }
 
     static void testTime(UnionFind uf) {
---
+++ 02-并查集/src/com/ylc/union/UnionFind_QU_R_PH.java
@@ -0,0 +1,22 @@
+package com.ylc.union;
+
+/**
+ * Quick Union -基于rank优化 -路径减半
+ */
+public class UnionFind_QU_R_PH extends UnionFind_QU_R {
+
+    public UnionFind_QU_R_PH(int capacity) {
+        super(capacity);
+    }
+
+    @Override
+    public int find(int v) {
+        rangeCheck(v);
+
+        while (v != parents[v]) {
+            parents[v] = parents[parents[v]];
+            v = parents[v];
+        }
+        return v;
+    }
+}
