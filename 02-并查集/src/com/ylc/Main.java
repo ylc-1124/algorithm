315369e19ecf31c8381d1665b3d31e635f6b882c
ylc-1124 <853703920@qq.com>
2021-08-18 17:39:08 +0800

    泛型的并查集

--- 02-并查集/src/com/ylc/Main.java
+++ 02-并查集/src/com/ylc/Main.java
@@ -9,11 +9,31 @@ public class Main {
     public static void main(String[] args) {
 //        testTime(new UnionFind_QF(COUNT));
 //        testTime(new UnionFind_QU(COUNT));
-        testTime(new UnionFind_QU_S(COUNT));
-        testTime(new UnionFind_QU_R(COUNT));
-        testTime(new UnionFind_QU_R_PC(COUNT));
-        testTime(new UnionFind_QU_R_PS(COUNT));
-        testTime(new UnionFind_QU_R_PH(COUNT));
+//        testTime(new UnionFind_QU_S(COUNT));
+//        testTime(new UnionFind_QU_R(COUNT));
+//        testTime(new UnionFind_QU_R_PC(COUNT));
+//        testTime(new UnionFind_QU_R_PS(COUNT));
+//        testTime(new UnionFind_QU_R_PH(COUNT));
+
+        GenericUnionFind<Student> uf = new GenericUnionFind<>();
+		Student stu1 = new Student(1, "jack");
+		Student stu2 = new Student(2, "rose");
+		Student stu3 = new Student(3, "jack");
+		Student stu4 = new Student(4, "rose");
+		uf.makeSet(stu1);
+		uf.makeSet(stu2);
+		uf.makeSet(stu3);
+		uf.makeSet(stu4);
+
+		uf.union(stu1, stu2);
+		uf.union(stu3, stu4);
+
+		uf.union(stu1, stu4);
+
+		Asserts.test(uf.isSame(stu2, stu3));
+		Asserts.test(uf.isSame(stu3, stu4));
+		Asserts.test(uf.isSame(stu1, stu3));
+
 
     }
 
---
+++ 02-并查集/src/com/ylc/Student.java
@@ -0,0 +1,11 @@
+package com.ylc;
+
+public class Student {
+    private int age;
+    private String name;
+
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+}
