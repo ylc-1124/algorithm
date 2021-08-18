508daef8e7a914d4b68003d957d8b117f31559d2
ylc-1124 <853703920@qq.com>
2021-08-18 14:25:39 +0800

    Quick Union两种优化

---
+++ 02-并查集/src/com/ylc/union/UnionFind_QU_S.java
@@ -0,0 +1,46 @@
+package com.ylc.union;
+
+/**
+ * Quick Union 基于size的优化
+ */
+public class UnionFind_QU_S extends UnionFind {
+    private int[] sizes;
+
+    public UnionFind_QU_S(int capacity) {
+        super(capacity);
+        sizes = new int[capacity];
+        for (int i = 0; i < sizes.length; i++) {
+            sizes[i] = 1;
+        }
+    }
+
+    /**
+     * 查找v的根节点，父节点就是根节点
+     */
+    @Override
+    public int find(int v) {
+        rangeCheck(v);
+        while (v != parents[v]) {
+            v = parents[v];
+        }
+        return v;
+    }
+
+    /**
+     * 将v1的根节点嫁接到v2的根节点上
+     */
+    @Override
+    public void union(int v1, int v2) {
+        int p1 = find(v1);
+        int p2 = find(v2);
+        if (p1 == p2) return;
+
+        if (sizes[p1] < sizes[p2]) {
+            parents[p1] = p2;
+            sizes[p2] += sizes[p1];
+        } else {
+            parents[p2] = p1;
+            sizes[p1] += sizes[p2];
+        }
+    }
+}
